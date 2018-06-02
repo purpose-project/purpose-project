4ed08b9af53fcea8ad1571b58efe21de085b0e8c
Sean Stamm <sean.stamm5@gmail.com>
2018-06-02 12:00:09 -0400

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'getting-started', component: StartComponent },
+  { path: 'welcome',             component: WelcomeComponent },
+  { path: '',  redirectTo: '/welcome', pathMatch: 'full' },
+  { path: '**',                  component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
