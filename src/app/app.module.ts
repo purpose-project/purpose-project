42e3384569a9aa11dad333e79f2fa39acc1af565
Sean Stamm <sean.stamm5@gmail.com>
2018-06-04 11:46:24 -0400

    Add module for start pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BeginModule } from './begin/begin.module';
 
-import { StartComponent } from './start/start.component';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent,
-    WelcomeComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    BeginModule,
     AppRoutingModule
   ],
   providers: [],
