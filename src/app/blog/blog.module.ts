7099f0b6521f3b95cdb628bacb2a4a6e5f2b652b
Sean Stamm <sean.stamm5@gmail.com>
2018-06-19 16:48:18 -0400

    Add post component, move blog component

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
-import { BlogComponent } from '../blog/blog.component';
+import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BlogRoutingModule
   ],
-  declarations: [BlogComponent]
+  declarations: [BlogComponent, PostComponent]
 })
 export class BlogModule { }
