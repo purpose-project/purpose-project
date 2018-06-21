e889648c31a50205e997a756093b90444464c45f
Sean Stamm <sean.stamm5@gmail.com>
2018-06-21 16:37:47 -0400

    Add button component to shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule
   ],
-  declarations: [],
+  declarations: [ButtonComponent],
   exports: [
     CommonModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    ButtonComponent
   ]
 })
 export class SharedModule { }
