53b9d8f430c8e09745d50ad4b9c02f53c7cd64a8
Sean Stamm <seanstamm@christinas-mbp.lan>
2018-07-07 21:31:59 -0400

    Add wordpress post

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
+  { path: 'post/:slug', component: PostComponent },
   { path: 'blog', component: BlogComponent }
 ];
 
--- src/app/blog/post/post.component.ts
+++ src/app/blog/post/post.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { PageTitleService } from '../../page-title.service';
 import { ContentfulService } from '../../contentful.service';
+import { Post } from '../post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post',
@@ -12,18 +16,29 @@ import { ContentfulService } from '../../contentful.service';
 })
 export class PostComponent implements OnInit {
 
-  post$: Observable<any>;
+  post$: Observable<Post>;
 
   constructor(
     private titleService: PageTitleService,
+    private route: ActivatedRoute,
+    private postService: PostService,
     private contentful: ContentfulService
   ) { }
 
   ngOnInit() {
-    this.contentful.logContent('4d1s24xc6QAK8cyeMiQs2c');
+    this.post$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.postService.getPost(params.get('slug')))
+    );
 
-    this.post$ = this.contentful.getContent('4d1s24xc6QAK8cyeMiQs2c');
-    this.post$.subscribe(post => this.titleService.setTitle(post.title));
+    // this.contentful.logContent('4d1s24xc6QAK8cyeMiQs2c');
+
+    // this.post$ = this.contentful.getContent('4d1s24xc6QAK8cyeMiQs2c');
+    this.post$.subscribe(post => {
+      console.log(post);
+      this.titleService.setTitle(post.title);
+
+    });
   }
 
 }
