1881eced228216121848aa42a841089a2f9762bf
Sean Stamm <seanstamm@christinas-mbp.lan>
2018-07-07 21:28:27 -0400

    Udate module

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BlogRoutingModule } from './blog-routing.module';
+import { PostService } from './post.service';
 import { BlogComponent } from './blog/blog.component';
+import { CategoryComponent } from './category/category.component';
 import { PostComponent } from './post/post.component';
 import { MdToHtmlPipe } from './md-to-html.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     BlogRoutingModule
+  ],
+  providers: [
+    PostService
   ],
   declarations: [
     BlogComponent,
+    CategoryComponent,
     PostComponent,
     MdToHtmlPipe
   ]
