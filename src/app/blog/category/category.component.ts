e0603aa27f1e4b2cae6d3b5d8ac60560b506eb8e
Sean Stamm <sean.stamm5@gmail.com>
2018-08-01 16:22:54 -0700

    Have category set background image

--- src/app/blog/category/category.component.ts
+++ src/app/blog/category/category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
+import { PageTitleService } from '../../page-title.service';
 import { PostService } from '../post.service';
 import { Post } from '../post';
 
@@ -12,10 +13,14 @@ import { Post } from '../post';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor(private postService: PostService) { }
+  constructor(
+    private postService: PostService,
+    private titleService: PageTitleService
+  ) { }
   posts$: Observable<Post[]>;
 
   ngOnInit() {
     this.posts$ = this.postService.getPosts();
+    this.titleService.setBackgroundImage('https://source.unsplash.com/gD7KrKqYZFk/1600x900');
   }
 }
