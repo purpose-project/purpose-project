2f29d9d467e5c7b1a3f3f3e8fa7d41253371fec2
Sean Stamm <seanstamm@Christinas-MacBook-Pro.local>
2018-09-05 22:43:03 -0700

    Add response to start steps based on user

--- src/app/modules/begin/start/start.component.ts
+++ src/app/modules/begin/start/start.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { User } from 'firebase';
 
 import { StepService } from './service/step.service';
 import { Step } from './service/step';
@@ -15,18 +19,37 @@ import { PageTitleService } from '../../../page-title.service';
 })
 export class StartComponent implements OnInit {
 
+  private itemDoc: AngularFirestoreDocument<any>;
+
   step$: Observable<Step>;
+  user: User;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private stepService: StepService,
-    private pageTitleService: PageTitleService
+    private pageTitleService: PageTitleService,
+    private afAuth: AngularFireAuth,
+    private afFirestore: AngularFirestore
   ) { }
 
   ngOnInit() {
-    this.getStep();
+    // Set page title
     this.pageTitleService.setTitle('Find your purpose');
+    // Get the step we are on
+    this.getStep();
+    // Get user
+    this.afAuth.user.subscribe(user => {
+      if(user) {
+        this.user = user;
+      } else {
+        console.log('no user logged in');
+      }
+    });
+  }
+
+  public setStepResponse(val: string): void {
+    let stepDoc = this.afFirestore.doc(`users/${this.user.uid}/goals/ideal_life`);
+    stepDoc.set({data: val});
   }
 
   private getStep(): void {
